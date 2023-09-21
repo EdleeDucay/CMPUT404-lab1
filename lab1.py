8d7408273c8eaa3254e32c363a1cb9806e7d50e4
edleeducay <ducay@ualberta.ca>
2023-09-20 19:39:53 -0600

    modify lab1

--- lab1.py
+++ lab1.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
 
-resp = requests.get("http://www.google.com")
+resp = requests.get("https://raw.githubusercontent.com/EdleeDucay/CMPUT404-lab1/main/lab1.py")
 
 print(resp.text)
